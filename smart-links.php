d26bb2f37b91a6bf64bf57352741715839dfa888
Denis de Bernardy <ddebarnardy@yahoo.com>
2010-03-05 14:26:57 +0000

    bump
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/smart-links/trunk@3993 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- smart-links.php
+++ smart-links.php
@@ -4,7 +4,7 @@ Plugin Name: Smart Links
 Plugin URI: http://www.semiologic.com/software/smart-links/
 Description: Lets you write links as [link text->link ref] (explicit link), or as [link text->] (implicit link).
 Author: Denis de Bernardy
-Version: 4.2.3 beta
+Version: 4.2.3
 Author URI: http://www.getsemiologic.com
 Text Domain: smart-links
 Domain Path: /lang
